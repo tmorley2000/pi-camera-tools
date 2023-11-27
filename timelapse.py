1894928fec8cf806a61b5261a5cc5f7537fff592
Tim Morley <tim@chaos.org.uk>
2023-11-27 16:55:12 +0000

    First step on a simple timelapse too

---
+++ timelapse.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import time
+import datetime
+import sys
+
+import cv2
+
+from picamera2 import MappedArray, Picamera2
+from picamera2.encoders import H264Encoder
+
+fd=15.0
+
+picam2 = Picamera2(tuning="imx477_scientific-20sec.json")
+
+controls={"FrameDurationLimits": (int(fd*1000000), int(fd*1000000))}
+sc=picam2.create_still_configuration({'size':(2028,1520),'format':'XBGR8888'},controls=controls,buffer_count=2)
+
+#sys.exit(0)
+
+picam2.configure(sc)
+
+foreground = (255, 255, 255)
+background = (0,0,0)
+
+origin = (0, 30)
+font = cv2.FONT_HERSHEY_SIMPLEX
+scale = 0.7
+thickness = 2
+
+
+
+def apply_timestamp(request,dt):
+    md=request.get_metadata()
+    text=[]
+    text.append(dt.isoformat())
+    text.append("EXP: %f AG: %f DG: %f"%(md['ExposureTime']/1000000,md['AnalogueGain'],md['DigitalGain']))
+    text.append("TEMP: %f LUX: %f CT: %d Focus: %d"%(md['SensorTemperature'],md['Lux'],md['ColourTemperature'],md['FocusFoM']))
+
+    with MappedArray(request, "main") as m:
+        y=-2
+        for a in text:
+            (w, h), baseline=cv2.getTextSize(a,font, scale, thickness)
+            y+=h+6
+            cv2.rectangle(m.array,(0,y+1+baseline),(w+2,y-h-2),background,-1)
+            cv2.putText(m.array, a, (1,y), font, scale, foreground, thickness)
+        
+
+
+x=0
+picam2.start()
+while True:
+    r=picam2.capture_request()
+    dt=datetime.datetime.utcnow()
+    apply_timestamp(r,dt)
+    r.save("main",dt.isoformat()+".jpg")
+    r.release()
