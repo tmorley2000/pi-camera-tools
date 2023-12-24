97d0ae914915d57f79bc66e0404fe839dbbd5cc4
Tim Morley <tim@chaos.org.uk>
2023-12-24 11:32:20 +0000

    Argument parsing, 'latest' file, and better filename generation

--- timelapse.py
+++ timelapse.py
@@ -2,6 +2,8 @@
 import time
 import datetime
 import sys
+import os
+import argparse
 
 import cv2
 
@@ -9,21 +11,30 @@ from picamera2 import MappedArray, Picamera2
 from libcamera import controls
 from picamera2.encoders import H264Encoder
 
-fd=15.0
+parser = argparse.ArgumentParser(description='Pi Timelapse', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--interval', type=int, default=15, help='Timelapse interval (s)')
+parser.add_argument('--maxgain', type=float, default=12.0, help='Maximum gain')
+parser.add_argument('--dirname', type=str, default="imgs/", help='Directory to save images')
+parser.add_argument('--filename', type=str, default="%Y/%m/%d/%Y%m%dT%H%M%S.png", help='Filename template (parsed with strftime, directories automatically created)')
+parser.add_argument('--latest', type=str, default="latest.png", help='Name of file to symlink latest image to')
+parser.add_argument('--tuningfile', type=str, default="imx477_scientific.json", help='Base tuning file for camera, AGC parameters will be overridden')
 
-tuning = Picamera2.load_tuning_file("imx477_scientific.json")
+#parser.add_argument('--font', type=str, default='/usr/share/fonts/truetype/ttf-bitstream-vera/VeraBd.ttf', help='TTF font file for overlay text')
+#parser.add_argument('--fontsize', type=int, default=12, help='Font size for overlay text')
+
+args = parser.parse_args()
+
+tuning = Picamera2.load_tuning_file(args.tuningfile)
 algo = Picamera2.find_tuning_algo(tuning, "rpi.agc")
 if "channels" in algo:
-    algo["channels"][0]["exposure_modes"]["normal"] = {"shutter": [100,int(fd*1000000)], "gain": [1.0,12.0]}
+    algo["channels"][0]["exposure_modes"]["normal"] = {"shutter": [100,int(args.interval*1000000)], "gain": [1.0,args.maxgain]}
 else:
-    algo["exposure_modes"]["normal"] = {"shutter": [100,int(fd*1000000)], "gain": [1.0,12.0]}
+    algo["exposure_modes"]["normal"] = {"shutter": [100,int(args.interval*1000000)], "gain": [1.0,args.maxgain]}
 
-
-#picam2 = Picamera2(tuning="imx477_scientific-20sec.json")
 picam2 = Picamera2(tuning=tuning)
 
 crtl={}
-crtl["FrameDurationLimits"]= (int(fd*1000000), int(fd*1000000))
+crtl["FrameDurationLimits"]= (int(args.interval*1000000), int(args.interval*1000000))
 # Off Fast HighQuality
 crtl["NoiseReductionMode"]=controls.draft.NoiseReductionModeEnum.Off
 sc=picam2.create_still_configuration({'size':(2028,1520),'format':'XBGR8888'},controls=crtl,buffer_count=2)
@@ -65,5 +76,11 @@ while True:
     r=picam2.capture_request()
     dt=datetime.datetime.utcnow()
     apply_timestamp(r,dt)
-    r.save("main",dt.isoformat()+".jpg")
+    filename=dt.strftime(args.filename)
+    if '/' in filename:
+        os.makedirs(os.path.dirname(os.path.join(args.dirname,filename)),exist_ok=True)
+    r.save("main",os.path.join(args.dirname,filename))
+    os.symlink(filename,os.path.join(args.dirname,args.latest+".new"))
+    os.rename(os.path.join(args.dirname,args.latest+".new"),os.path.join(args.dirname,args.latest))
+
     r.release()
