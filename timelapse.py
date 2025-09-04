12446be0c920a15e3031fd6150350ccebf732245
Tim Morley <tim@chaos.org.uk>
2025-09-04 17:52:29 +0100

    New options and better tuning file handling for timelapses

--- timelapse.py
+++ timelapse.py
@@ -13,40 +13,87 @@ import json
 import cv2
 
 from picamera2 import MappedArray, Picamera2
-from libcamera import controls
+from libcamera import controls, Transform
 from picamera2.encoders import H264Encoder
 
 parser = argparse.ArgumentParser(description='Pi Timelapse', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--interval', type=int, default=15, help='Timelapse interval (s)')
+parser.add_argument('--binning', type=int, default=2, help='Downscale sensor resolution')
 parser.add_argument('--maxgain', type=float, default=12.0, help='Maximum gain')
 parser.add_argument('--dirname', type=str, default="imgs/", help='Directory to save images')
 parser.add_argument('--filename', type=str, default="%Y/%m/%d/%Y%m%dT%H%M%S.jpg", help='Filename template (parsed with strftime, directories automatically created)')
 parser.add_argument('--latest', type=str, default="latest.jpg", help='Name of file to symlink latest image to')
 parser.add_argument('--metadata', type=str, default="%Y/%m/%d/metadata.json", help='Separate dump of image metadata')
-parser.add_argument('--tuningfile', type=str, default="imx477_scientific.json", help='Base tuning file for camera, AGC parameters will be overridden')
+parser.add_argument('--tuningfile', type=str, default=None, help='Base tuning file for camera, AGC parameters will be overridden')
+parser.add_argument('--camera', type=int, default=0, help='Camera Number')
+parser.add_argument('--rotate', default=False, help='Rotate image 180', action='store_true')
+
+syncgroup = parser.add_mutually_exclusive_group()
+syncgroup.add_argument('--syncserver', default=False, help='Camera Sync, server mode', action='store_true')
+syncgroup.add_argument('--syncclient', default=False, help='Camera Sync, client mode', action='store_true')
+
+parser.add_argument('--syncreadyframe', type=int, default=None, help='How many frames for sync server to wait before declaring itself ready')
+parser.add_argument('--syncperiod', type=int, default=None, help='How often the sync server should advertise timing')
 
 #parser.add_argument('--font', type=str, default='/usr/share/fonts/truetype/ttf-bitstream-vera/VeraBd.ttf', help='TTF font file for overlay text')
 #parser.add_argument('--fontsize', type=int, default=12, help='Font size for overlay text')
 
+parser.add_argument('--debug', default=False, help='Debug mode', action='store_true')
+
 args = parser.parse_args()
 
+
+
+if args.tuningfile is None:
+    allcams=Picamera2.global_camera_info()
+
+    # Seems global_camera_info initialises the libcamera object so 
+    # new tuning file doesn't work!
+    # Following from https://github.com/raspberrypi/picamera2/issues/886
+    from picamera2.picamera2 import CameraManager
+    import gc
+    del Picamera2._cm
+    Picamera2._cm = None
+    gc.collect()
+    Picamera2._cm = CameraManager()
+
+    for c in allcams:
+        if c['Num']==args.camera:
+            args.tuningfile=c['Model']+'.json'
+
 tuning = Picamera2.load_tuning_file(args.tuningfile)
-algo = Picamera2.find_tuning_algo(tuning, "rpi.agc")
-if "channels" in algo:
-    algo["channels"][0]["exposure_modes"]["normal"] = {"shutter": [100,int(args.interval*1000000)], "gain": [1.0,args.maxgain]}
+agc = Picamera2.find_tuning_algo(tuning, "rpi.agc")
+if "channels" in agc:
+    agc["channels"][0]["exposure_modes"]["normal"] = {"shutter": [100,int(args.interval*1000000)], "gain": [1.0,args.maxgain]}
 else:
-    algo["exposure_modes"]["normal"] = {"shutter": [100,int(args.interval*1000000)], "gain": [1.0,args.maxgain]}
+    agc["exposure_modes"]["normal"] = {"shutter": [100,int(args.interval*1000000)], "gain": [1.0,args.maxgain]}
 
-picam2 = Picamera2(tuning=tuning)
+sync=Picamera2.find_tuning_algo(tuning, "rpi.sync")
+if args.syncreadyframe:
+    sync["ready_frame"]=args.syncreadyframe
+if args.syncperiod:
+    sync["sync_period"]=5
 
-crtl={}
-crtl["FrameDurationLimits"]= (int(args.interval*1000000), int(args.interval*1000000))
+picam2 = Picamera2(tuning=tuning,camera_num=args.camera)
+
+ctrls={}
+ctrls["FrameDurationLimits"]= (int(args.interval*1000000), int(args.interval*1000000))
 # Off Fast HighQuality
-crtl["NoiseReductionMode"]=controls.draft.NoiseReductionModeEnum.Off
+ctrls["NoiseReductionMode"]=controls.draft.NoiseReductionModeEnum.Off
 
-size=tuple(x>>1 for x in picam2.sensor_resolution)
+if args.syncserver:
+    ctrls['SyncMode']=controls.rpi.SyncModeEnum.Server
+elif args.syncclient:
+    ctrls['SyncMode']=controls.rpi.SyncModeEnum.Client
 
-sc=picam2.create_still_configuration({'size':size,'format':'XBGR8888'},controls=crtl,buffer_count=2)
+if args.rotate:
+    transform=Transform(hflip=True, vflip=True)
+else:
+    transform=Transform()
+
+size=tuple(int(x/args.binning) for x in picam2.sensor_resolution)
+
+sc=picam2.create_still_configuration({'size':size,'format':'XBGR8888'},controls=ctrls,buffer_count=2,transform=transform)
 
 picam2.configure(sc)
 
@@ -60,6 +107,8 @@ thickness = 2
 
 def apply_timestamp(request,dt):
     md=request.get_metadata()
+    if args.debug:
+        print("Cam %d exp %f ag %f dg %f"%(args.camera,md['ExposureTime']/1000000,md['AnalogueGain'],md['DigitalGain']))
     text=[]
     text.append(dt.isoformat())
     text.append("EXP: %f AG: %f DG: %f"%(md['ExposureTime']/1000000,md['AnalogueGain'],md['DigitalGain']))
